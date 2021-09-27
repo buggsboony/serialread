1328af5aed4d518467fe8e6d15c4f4a0021494a4
buggsboony <boonydeboon@gmail.com>
2021-09-26 23:07:39 -0400

    Aouch, big mistake in installation file REPARED !

--- serialread.py
+++ serialread.py
@@ -1,1 +1,7 @@
-/usr/bin/serialread -
+import serial
+
+print('Serial read is running')
+arduino = serial.Serial('/dev/ttyUSB0', baudrate=115200, timeout=1.0)   
+while(True):
+ line = arduino.readline().decode('utf-8')
+ print(line)
